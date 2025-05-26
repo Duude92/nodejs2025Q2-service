e7e04eefb506c20cad8e1cebe1403aa5a66d20a6
Duude92 <switch4acc@gmail.com>
2025-05-26 12:20:19 +0300

    Using of users repository

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { User } from './user.entity';
 
 @Injectable()
 export class UserService {
-  getUsers(): string {
-    return 'users';
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async getUsers(): Promise<User[]> {
+    return [await this.userRepository.findOne({ where: { login: 'admin' } })];
   }
 }
