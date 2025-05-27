29f83f24e39a715a58b29bf633682d43a8a1ea89
Duude92 <switch4acc@gmail.com>
2025-05-27 14:24:11 +0300

    Fix clearing track.albumId before removing album

--- src/album/album.module.ts
+++ src/album/album.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AlbumService } from './album.service';
 import { AlbumController } from './album.controller';
 import { AlbumRepository } from '../repositories/album.repository';
+import { TrackRepository } from '../repositories/track.repository';
 
 @Module({
   controllers: [AlbumController],
-  providers: [AlbumService, AlbumRepository],
+  providers: [AlbumService, AlbumRepository, TrackRepository],
 })
 export class AlbumModule {}
--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -3,10 +3,14 @@ import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
 import { AlbumRepository } from '../repositories/album.repository';
 import { createAlbum } from './entities/album.entity';
+import { TrackRepository } from '../repositories/track.repository';
 
 @Injectable()
 export class AlbumService {
-  constructor(private readonly albumRepository: AlbumRepository) {}
+  constructor(
+    private readonly albumRepository: AlbumRepository,
+    private readonly trackRepository: TrackRepository,
+  ) {}
 
   async create(createAlbumDto: CreateAlbumDto) {
     return await this.albumRepository.save(createAlbum(createAlbumDto));
@@ -30,6 +34,11 @@ export class AlbumService {
   }
 
   async remove(id: string) {
+    const tracks = await this.trackRepository.find({ where: { albumId: id } });
+    tracks.forEach((track) => {
+      track.albumId = null;
+      this.trackRepository.save(track);
+    });
     return await this.albumRepository.delete(id);
   }
 }
