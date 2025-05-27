4613521c7128cb9e10434a588185843c9455cdfe
Duude92 <switch4acc@gmail.com>
2025-05-27 11:43:16 +0300

    Declaration of artist entity and create-artist dto

--- src/artist/entities/artist.entity.ts
+++ src/artist/entities/artist.entity.ts
@@ -1,1 +1,22 @@
-export class Artist {}
+import { IsBoolean, IsNotEmpty, IsString, IsUUID } from 'class-validator';
+import { CreateArtistDto } from '../dto/create-artist.dto';
+import { randomUUID } from 'node:crypto';
+
+export class Artist {
+  @IsUUID()
+  id: string; // uuid v4
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+  @IsNotEmpty()
+  @IsBoolean()
+  grammy: boolean;
+
+  constructor(artistDto: CreateArtistDto) {
+    this.id = randomUUID();
+    this.name = artistDto.name;
+    this.grammy = artistDto.grammy;
+  }
+}
+
+export const createArtist = (data: CreateArtistDto) => new Artist(data);
