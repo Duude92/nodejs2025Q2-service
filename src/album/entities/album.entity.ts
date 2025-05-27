7a56d7441f4f55f7e4343b91da0996e388bc4dee
Duude92 <switch4acc@gmail.com>
2025-05-27 13:59:48 +0300

    Declaration of Album entity / create album dto

--- src/album/entities/album.entity.ts
+++ src/album/entities/album.entity.ts
@@ -1,1 +1,13 @@
-export class Album {}
+import { IsNotEmpty, IsNumber, IsString, IsUUID } from 'class-validator';
+
+export class Album {
+  @IsUUID()
+  id: string; // uuid v4
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+  @IsNotEmpty()
+  @IsNumber()
+  year: number;
+  artistId: string | null; // refers to Artist
+}
