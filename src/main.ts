58144ff4710611df9d8236fcaee665b0118d9d7c
Duude92 <switch4acc@gmail.com>
2025-05-28 14:20:26 +0300

    Install swagger/setup

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { APP_PORT } from './appconfig';
 import { ValidationPipe } from '@nestjs/common';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'node:fs/promises';
+import { parse } from 'yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const ddd = parse(await readFile('./doc/api.yaml', 'utf8')) as OpenAPIObject;
+  SwaggerModule.setup('doc', app, ddd);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(APP_PORT);
 }
