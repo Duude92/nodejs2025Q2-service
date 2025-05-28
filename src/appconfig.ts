1710c70c44b3238b686848eda5268ff030f78dc8
Duude92 <switch4acc@gmail.com>
2025-05-28 17:37:56 +0300

    Expose pg credentials into application

--- src/appconfig.ts
+++ src/appconfig.ts
@@ -9,3 +9,5 @@ export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 export const JWT_SECRET_REFRESH_KEY = process.env.JWT_SECRET_REFRESH_KEY;
 export const TOKEN_EXPIRE_TIME = process.env.TOKEN_EXPIRE_TIME;
 export const TOKEN_REFRESH_EXPIRE_TIME = process.env.TOKEN_REFRESH_EXPIRE_TIME;
+export const PG_USER = process.env.PG_USER;
+export const PG_PASSWD = process.env.PG_PASSWD;
