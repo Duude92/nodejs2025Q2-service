2004be14a019e381779183a2d24db0ca793cf022
Duude92 <switch4acc@gmail.com>
2025-05-28 09:59:53 +0300

    Declaration of fav entity

--- src/favs/entities/fav.entity.ts
+++ src/favs/entities/fav.entity.ts
@@ -1,1 +1,15 @@
-export class Fav {}
+import { IsString } from 'class-validator';
+
+export class Favorites {
+  @IsString({ each: true })
+  artists: string[]; // favorite artists ids
+  @IsString({ each: true })
+  albums: string[]; // favorite albums ids
+  @IsString({ each: true })
+  tracks: string[]; // favorite tracks ids
+  constructor() {
+    this.artists = [];
+    this.albums = [];
+    this.tracks = [];
+  }
+}
