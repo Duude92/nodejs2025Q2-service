f5ec476432516b159dca306949f703e2422e8229
Duude92 <switch4acc@gmail.com>
2025-05-29 11:24:03 +0300

    Update favourite entity

--- src/favs/entities/fav.entity.ts
+++ src/favs/entities/fav.entity.ts
@@ -3,14 +3,20 @@ import { randomUUID } from 'node:crypto';
 import { Artist } from '../../artist/entities/artist.entity';
 import { Album } from '../../album/entities/album.entity';
 import { Track } from '../../track/entities/track.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 export class Favorites {
   @IsUUID()
+  @PrimaryGeneratedColumn('uuid')
   readonly id: string;
+  @Column({ type: 'text', array: true })
   @IsString({ each: true })
   artists: string[]; // favorite artists ids
+  @Column({ type: 'text', array: true })
   @IsString({ each: true })
   albums: string[]; // favorite albums ids
+  @Column({ type: 'text', array: true })
   @IsString({ each: true })
   tracks: string[]; // favorite tracks ids
   constructor() {
