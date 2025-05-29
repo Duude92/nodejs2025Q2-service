a9cb92839960b071db45b727d536b0df78e738b3
Duude92 <switch4acc@gmail.com>
2025-05-29 10:21:14 +0300

    Update track entity for db

--- src/track/entities/track.entity.ts
+++ src/track/entities/track.entity.ts
@@ -2,21 +2,31 @@ import { IEntity } from '../../repositories/IEntity';
 import { IsUUID } from 'class-validator';
 import { CreateTrackDto } from '../dto/create-track.dto';
 import { randomUUID } from 'node:crypto';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 export class Track implements IEntity {
   @IsUUID('4', { each: false })
+  @PrimaryGeneratedColumn('uuid')
   id: string; // uuid v4
+  @Column()
   name: string;
+  @Column({ type: 'text', nullable: true })
   artistId: string | null; // refers to Artist
+  @Column({ type: 'text', nullable: true })
   albumId: string | null; // refers to Album
+  @Column()
   duration: number; // integer number
-  constructor({ name, artistId, albumId, duration }: CreateTrackDto) {
-    this.id = randomUUID();
-    this.name = name;
-    this.artistId = artistId;
-    this.albumId = albumId;
-    this.duration = duration;
-  }
 }
 
-export const createTrack = (track: CreateTrackDto) => new Track(track);
+const createTrackWithDto = (dto: CreateTrackDto) => {
+  const track = new Track();
+  track.id = randomUUID();
+  track.name = dto.name;
+  track.artistId = dto.artistId;
+  track.albumId = dto.albumId;
+  track.duration = dto.duration;
+  return track;
+};
+
+export const createTrack = (track: CreateTrackDto) => createTrackWithDto(track);
