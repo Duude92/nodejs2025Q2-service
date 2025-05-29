e383208a70ac50463c2851693801931056f2f9bc
Duude92 <switch4acc@gmail.com>
2025-05-29 10:59:45 +0300

    Usage of Artist Repository

--- src/artist/artist.module.ts
+++ src/artist/artist.module.ts
@@ -1,14 +1,19 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { ArtistService } from './artist.service';
 import { ArtistController } from './artist.controller';
-import { ArtistRepository } from '../repositories/artist.repository';
 import { TrackModule } from '../track/track.module';
 import { AlbumModule } from '../album/album.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Artist } from './entities/artist.entity';
 
 @Module({
-  imports: [forwardRef(() => TrackModule), forwardRef(() => AlbumModule)],
+  imports: [
+    forwardRef(() => TrackModule),
+    forwardRef(() => AlbumModule),
+    TypeOrmModule.forFeature([Artist]),
+  ],
   controllers: [ArtistController],
-  providers: [ArtistService, ArtistRepository],
+  providers: [ArtistService],
   exports: [ArtistService],
 })
 export class ArtistModule {}
