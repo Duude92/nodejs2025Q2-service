4bac46daa971d23c7c216e7e860f88b8b5230a7c
Duude92 <switch4acc@gmail.com>
2025-05-30 11:58:24 +0300

    Added relations/join columns per track

--- src/track/entities/track.entity.ts
+++ src/track/entities/track.entity.ts
@@ -2,7 +2,15 @@ import { IEntity } from '../../repositories/IEntity';
 import { IsUUID } from 'class-validator';
 import { CreateTrackDto } from '../dto/create-track.dto';
 import { randomUUID } from 'node:crypto';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Artist } from '../../artist/entities/artist.entity';
+import { Album } from '../../album/entities/album.entity';
 
 @Entity()
 export class Track implements IEntity {
@@ -11,8 +19,14 @@ export class Track implements IEntity {
   id: string; // uuid v4
   @Column()
   name: string;
+  @OneToOne(() => Artist)
+  @JoinColumn({ name: 'artistId' })
+  artist: Artist | null;
   @Column({ type: 'text', nullable: true })
   artistId: string | null; // refers to Artist
+  @OneToOne(() => Album, (album) => album.id)
+  @JoinColumn({ name: 'albumId' })
+  album: Album;
   @Column({ type: 'text', nullable: true })
   albumId: string | null; // refers to Album
   @Column()
