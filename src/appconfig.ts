e81124abe6a87c2543fb28db4f6d66e52a594d0a
Duude92 <switch4acc@gmail.com>
2025-06-01 14:35:19 +0300

    Fix db port parsing

--- src/appconfig.ts
+++ src/appconfig.ts
@@ -13,7 +13,7 @@ export const TOKEN_REFRESH_EXPIRE_TIME = process.env.TOKEN_REFRESH_EXPIRE_TIME;
 export const DB_CONNECTION = {
   host: process.env.PG_HOST,
   database: process.env.PG_DB,
-  port: parseInt(process.env.PG_PORT) | 5432,
+  port: parseInt(process.env.PG_PORT || '') || 5432,
   username: process.env.PG_USER,
   password: process.env.PG_PASSWD,
 };
