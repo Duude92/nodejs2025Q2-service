5d6ac36b2a9be47b68b6ae6cd2090603dcbfabc4
Duude92 <switch4acc@gmail.com>
2025-06-04 13:16:34 +0300

    Added configuration for logging

--- src/appconfig.ts
+++ src/appconfig.ts
@@ -2,6 +2,8 @@ import * as process from 'node:process';
 import { config } from 'dotenv';
 
 config();
+const isTrue = (value: string | undefined): boolean =>
+  value?.toLowerCase() === 'true';
 
 export const APP_PORT = parseInt(process.env.PORT || '') || 4000;
 export const CRYPT_SALT = process.env.CRYPT_SALT;
@@ -9,6 +11,12 @@ export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 export const JWT_SECRET_REFRESH_KEY = process.env.JWT_SECRET_REFRESH_KEY;
 export const TOKEN_EXPIRE_TIME = process.env.TOKEN_EXPIRE_TIME;
 export const TOKEN_REFRESH_EXPIRE_TIME = process.env.TOKEN_REFRESH_EXPIRE_TIME;
+
+export const LOGGING = {
+  CONSOLE_LOG: isTrue(process.env.ENABLE_CONSOLE_LOG) || true,
+  LOG_FILE: process.env.ENABLE_CONSOLE_LOG,
+  LOG_LEVEL: process.env.LOG_LEVEL || 'info',
+};
 
 export const DB_CONNECTION = {
   host: process.env.PG_HOST,
