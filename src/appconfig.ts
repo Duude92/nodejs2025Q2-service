cfead8641e758416141582940d6e395d44058656
Duude92 <switch4acc@gmail.com>
2025-06-04 21:39:38 +0300

    Fix appconfig AUTH values with providing default values

--- src/appconfig.ts
+++ src/appconfig.ts
@@ -11,10 +11,10 @@ export const APP_PORT = parseInt(process.env.PORT || '') || 4000;
 
 export const AUTH = {
   CRYPT_SALT: getInt(process.env.CRYPT_SALT, 10),
-  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
-  JWT_SECRET_REFRESH_KEY: process.env.JWT_SECRET_REFRESH_KEY,
-  TOKEN_EXPIRE_TIME: process.env.TOKEN_EXPIRE_TIME,
-  TOKEN_REFRESH_EXPIRE_TIME: process.env.TOKEN_REFRESH_EXPIRE_TIME,
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || 'secret123123',
+  JWT_SECRET_REFRESH_KEY: process.env.JWT_SECRET_REFRESH_KEY || 'secret123123',
+  TOKEN_EXPIRE_TIME: process.env.TOKEN_EXPIRE_TIME || '1h',
+  TOKEN_REFRESH_EXPIRE_TIME: process.env.TOKEN_REFRESH_EXPIRE_TIME || '24h',
 };
 
 export const LOGGING = {
