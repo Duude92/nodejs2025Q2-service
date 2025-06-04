8ec071688fdc54d90e5edb957e9efa8f1e04c375
Duude92 <switch4acc@gmail.com>
2025-06-04 17:30:06 +0300

    Usage of JwtModule to generate token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,19 @@ import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user/user.entity';
 import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AUTH } from '../appconfig';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => UserModule)],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    forwardRef(() => UserModule),
+    JwtModule.register({
+      global: true,
+      secret: AUTH.JWT_SECRET_KEY,
+      signOptions: { expiresIn: AUTH.TOKEN_EXPIRE_TIME },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
