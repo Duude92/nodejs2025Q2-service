085bf890bdeeea843fde1d1d0047ab64a7d01df5
Duude92 <switch4acc@gmail.com>
2025-06-04 16:27:20 +0300

    Export and usage of UserService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user/user.entity';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => UserModule)],
   controllers: [AuthController],
   providers: [AuthService],
 })
