74759d8d28b1e0c5afc4658681faaa9f48029cd0
Duude92 <switch4acc@gmail.com>
2025-06-04 14:09:26 +0300

    Added logging uncaughtException and unhandledRejection

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,15 @@ import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import { readFile } from 'node:fs/promises';
 import { parse } from 'yaml';
 import { Logger } from './common/logger/logger.service';
+import * as process from 'node:process';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule, {
-    logger: new Logger(),
+    logger: logger,
   });
+  process.on('uncaughtException', (err) => logger.fatal(err));
+  process.on('unhandledRejection', (err) => logger.fatal(err));
 
   const ddd = parse(await readFile('./doc/api.yaml', 'utf8')) as OpenAPIObject;
   SwaggerModule.setup('doc', app, ddd, {
