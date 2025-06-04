db5b73cbb75d8c5a57ebbf19712b44061c55ad15
Duude92 <switch4acc@gmail.com>
2025-06-04 16:19:44 +0300

    Import Repository<User> to auth module, implement basic login feature

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { LoginDto } from './dto/signup';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @Post('signup')
-  async signup(@Body() signupDto: LoginDto) {}
+  async signup(@Body() signupDto: LoginDto) {
+    return this.authService.signup(signupDto);
+  }
 
   @Post('login')
-  async login(@Body() signupDto: LoginDto) {}
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
 
   @Post('refresh')
   async refresh(@Body() { refreshToken }: { refreshToken: string }) {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/signup';
+import { compare } from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../user/user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async login(loginDto: LoginDto) {
+    const user = await this.userRepository.findOne({
+      where: {
+        login: loginDto.login,
+      },
+      select: ['password'],
+    });
+    return await compare(loginDto.password, user.password);
+  }
+
+  async signup(signupDto: LoginDto) {
+    return Promise.resolve(undefined);
+  }
+}
