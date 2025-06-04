c9c1f7202931311cfeeb31fdd17454ba150fcaca
Duude92 <switch4acc@gmail.com>
2025-06-04 15:37:25 +0300

    Organize auth variables under single namespace

--- src/appconfig.ts
+++ src/appconfig.ts
@@ -4,13 +4,18 @@ import { config } from 'dotenv';
 config();
 const isTrue = (value: string | undefined): boolean =>
   value?.toLowerCase() === 'true';
+const getInt = (value: string, defaultValue: number) =>
+  parseInt(value || '') || defaultValue;
 
 export const APP_PORT = parseInt(process.env.PORT || '') || 4000;
-export const CRYPT_SALT = process.env.CRYPT_SALT;
-export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
-export const JWT_SECRET_REFRESH_KEY = process.env.JWT_SECRET_REFRESH_KEY;
-export const TOKEN_EXPIRE_TIME = process.env.TOKEN_EXPIRE_TIME;
-export const TOKEN_REFRESH_EXPIRE_TIME = process.env.TOKEN_REFRESH_EXPIRE_TIME;
+
+export const AUTH = {
+  CRYPT_SALT: getInt(process.env.CRYPT_SALT, 10),
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  JWT_SECRET_REFRESH_KEY: process.env.JWT_SECRET_REFRESH_KEY,
+  TOKEN_EXPIRE_TIME: process.env.TOKEN_EXPIRE_TIME,
+  TOKEN_REFRESH_EXPIRE_TIME: process.env.TOKEN_REFRESH_EXPIRE_TIME,
+};
 
 export const LOGGING = {
   CONSOLE_LOG: isTrue(process.env.ENABLE_CONSOLE_LOG) || true,
