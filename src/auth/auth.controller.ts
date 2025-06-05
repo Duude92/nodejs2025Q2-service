1d5aa20e85e7294c6a3ac5adea62ce609683b8b5
Duude92 <switch4acc@gmail.com>
2025-06-05 11:42:09 +0300

    Added check for public endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,25 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { LoginDto } from './dto/signup';
 import { AuthService } from './auth.service';
+import { Public } from '../common/public/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @Post('signup')
   async signup(@Body() signupDto: LoginDto) {
     return this.authService.signup(signupDto);
   }
 
+  @Public()
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
 
+  @Public()
   @Post('refresh')
   async refresh(@Body() { refreshToken }: { refreshToken: string }) {}
 }
--- src/common/guard/rest.guard.ts
+++ src/common/guard/rest.guard.ts
@@ -6,10 +6,15 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { AUTH } from '../../appconfig';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../public/public.decorator';
 
 @Injectable()
 export class RestGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
 
   private extractTokenFromHeader(request: Request): string | undefined {
     const headers = request.headers as Headers & { authorization?: string };
@@ -18,6 +23,12 @@ export class RestGuard implements CanActivate {
   }
 
   async canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) return true;
+
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token)
