d8aac7c72ae21898cd0caa8b5b5ecb634f119f9b
Duude92 <switch4acc@gmail.com>
2025-06-06 19:16:16 +0300

    Implementation of response transformers

--- src/common/datatransformer/datatransformer.service.ts
+++ src/common/datatransformer/datatransformer.service.ts
@@ -1,28 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { ITransformer } from './ITransformer';
 import { LoginReqTransform } from './transformers/login.req';
+import { LoginResTransform } from './transformers/login.res';
 
 @Injectable()
 export class DataTransformerService {
   private readonly requestTransformers: Record<string, ITransformer<unknown>> =
     {};
+  private readonly responseTransformers: Record<string, ITransformer<unknown>> =
+    {};
 
   constructor() {
     this.addTransformer(LoginReqTransform);
+    this.addTransformer(LoginResTransform);
+  }
+
+  getTransformers(isRequest: boolean) {
+    return isRequest ? this.requestTransformers : this.responseTransformers;
   }
 
   addTransformer(transformer: ITransformer<unknown>) {
-    if (transformer.isRequest) {
-      if (!!this.requestTransformers[transformer.endpoint])
-        throw new Error(
-          `DUPLICATION: Transformer ${transformer.endpoint} already exists`,
-        );
-      this.requestTransformers[transformer.endpoint] = transformer;
-    }
+    const transformers = this.getTransformers(transformer.isRequest);
+
+    if (!!transformers[transformer.endpoint])
+      throw new Error(
+        `DUPLICATION: Transformer ${transformer.endpoint} already exists`,
+      );
+    transformers[transformer.endpoint] = transformer;
   }
 
   transform(input: unknown, endpoint: string, isRequest: boolean) {
-    const transformers = isRequest ? this.requestTransformers : null;
+    const transformers = this.getTransformers(isRequest);
     if (!transformers[endpoint]) return input;
     return transformers[endpoint].transform(input);
   }
