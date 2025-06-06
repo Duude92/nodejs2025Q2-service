eca845f5208216b0ee7f1efee283e36525ddfca3
Duude92 <switch4acc@gmail.com>
2025-06-06 16:49:04 +0300

    Implementation of response logging with interception

--- src/common/loggedinterceptor/logged.interceptor.ts
+++ src/common/loggedinterceptor/logged.interceptor.ts
@@ -4,15 +4,33 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { Logger } from '../logger/logger.service';
+import { Response, Request } from 'express';
 
 @Injectable()
 export class LoggedInterceptor implements NestInterceptor {
   constructor(private readonly logger: Logger) {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    this.logger.warn(context.getHandler().name);
-    return next.handle();
+    const http = context.switchToHttp();
+    const response = http.getResponse<Response>();
+    const request = http.getRequest<Request>();
+    const rCode = response.statusCode;
+    const message = response.statusMessage;
+
+    return next
+      .handle()
+      .pipe(
+        tap((data) =>
+          this.logger.logResponse(
+            request.method,
+            request.url,
+            rCode,
+            message,
+            data,
+          ),
+        ),
+      );
   }
 }
