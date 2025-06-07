a8bf94575ffaa162e74fbaef9e5f86e1f714af36
Duude92 <switch4acc@gmail.com>
2025-06-07 18:52:58 +0300

    Expose LOG_SIZE property for logger usage

--- src/appconfig.ts
+++ src/appconfig.ts
@@ -17,11 +17,29 @@ export const AUTH = {
   TOKEN_REFRESH_EXPIRE_TIME: process.env.TOKEN_REFRESH_EXPIRE_TIME || '24h',
 };
 
+enum SIZE_POSTFIX {
+  b = 1,
+  k = 1024,
+  m = k * 1024,
+  g = m * 1024,
+  t = g * 1024,
+}
+
 export const LOGGING = {
   CONSOLE_LOG: isTrue(process.env.ENABLE_CONSOLE_LOG) || true,
   LOG_FILE: process.env.LOG_FILE,
   LOG_LEVEL: process.env.LOG_LEVEL || 'info',
   LOG_FOLDER: process.env.LOG_FOLDER || 'logs',
+  get LOG_SIZE() {
+    const size = process.env.LOG_FILE_SIZE || '500K';
+    const lastChar = size[size.length - 1];
+    const isLastNan = isNaN(Number(lastChar));
+    const postfix = isLastNan ? lastChar : 'b';
+    const numSize = Number(
+      size.substring(0, size.length - (isLastNan ? 1 : 0)),
+    );
+    return numSize * SIZE_POSTFIX[postfix.toLowerCase()];
+  },
 };
 
 export const DB_CONNECTION = {
